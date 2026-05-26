8ea9195e219675d90cf2a9e4bbcd009ceb00a3b1
Andrea Rovea <rovea.andrea2003@gmail.com>
2026-05-26 15:06:26 +0200

    feat: Task entity, repository and domain types

---
+++ backend/src/main/java/com/andrearovea/tasktracker/task/TaskRepository.java
@@ -0,0 +1,8 @@
+package com.andrearovea.tasktracker.task;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByStatus(TaskStatus status);
+}
